b4a33e49b79e699eb21a84cce4a9ec0fa8abda2d
Bryon Vandiver <unicdk@gmail.com>
2013-11-25 22:15:35 -0800

    Fixes, works, hurrah

--- microcode/compile.js
+++ microcode/compile.js
@@ -29,9 +29,10 @@ module.exports = function(grunt) {
 	grunt.registerMultiTask('microcode', 'Generates parsers from PEG grammars.', function() {
 		grunt.log.write('Compiling microcode', this.data.source, "\n");
 
-		var source = grunt.file.read(this.data.source),
-				structs = PEG.buildParser(grunt.file.read(this.data.structjs), this.data),
-				parser = PEG.buildParser(grunt.file.read(this.data.grammar), this.data),
+		var options = { output: "parser" },
+				source = grunt.file.read(this.data.source),
+				structs = PEG.buildParser(grunt.file.read(this.data.structjs), options),
+				parser = PEG.buildParser(grunt.file.read(this.data.grammar), options),
 				layout = structs.parse(grunt.file.read(this.data.layout)),
 				ast = parser.parse(source);
 
